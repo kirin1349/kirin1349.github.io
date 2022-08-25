29f86d497ae335f12992a843a66ddc1b2260330e
kirin1349 <kirin1349@gmail.com>
2022-08-26 02:53:30 +0800

    upload

---
+++ works/question/src/settings.bec5b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/GameEntry.fire",orientation:"",jsList:["assets/Scripts/Mini/Framework/miniframe_with_puremvc.3f9b6.js"],bundleVers:{internal:"52d36",resources:"472d7",main:"d7540"}};
